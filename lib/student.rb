48a4608dcc41e8369ce205488b42534b4f9c5cba
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-07-16 17:10:45 -0400

    commit tree b7bf0bb720722f01af251d401a1b7371df139271

--- lib/student.rb
+++ lib/student.rb
@@ -1,51 +1,69 @@
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+require 'pry'
 
 class Student
-  attr_accessor :name, :grade
+  attr_accessor :name, :length
   attr_reader :id
 
-  def initialize(name, grade, id=nil)
+  def initialize(name, length, id=nil)
     @name = name
-    @grade = grade
+    @length = length
     @id = id
   end
     
   def self.create_table
     sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS students (
+      CREATE TABLE IF NOT EXISTS songs (
         id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        grade INTEGER
+        title TEXT, 
+        length INTEGER
         )
         SQL
     DB[:conn].execute(sql) 
   end
     
   def self.drop_table
-    sql =  <<-SQL 
-    DROP TABLE students
-      SQL
+    sql =  "DROP TABLE students"
   DB[:conn].execute(sql)      
   end
 
-  def save
+  def self.new_from_db(row)
+    new_song = self.new  # self.new is the same as running Song.new
+    new_song.id = row[0]
+    new_song.name =  row[1]
+    new_song.length = row[2]
+    new_song  # return the newly created instance
+  end 
+
+  def self.create(name)
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
     SQL
 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+    binding.pry
+
+  end
+
+  def save
+    sql =  "INSERT INTO students (name, grade) VALUES (?, ?)"
     DB[:conn].execute(sql, self.name, self.grade)
 
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-
   end
 
-  def self.create(name:, grade:)
-    student = Student.new(name, grade)
-    student.save
-    student
-  end
+  # def self.create(name:, grade:)
+  #   student = Student.new(name, grade)
+  #   student.save
+  #   student
+  # end
+
 
 
 
